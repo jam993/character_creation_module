ca7894e9807572da7a459af7cb8106169176f142
Мартен Жан <martenzh@yandex.ru>
2022-10-26 17:09:49 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from random import randint
-
+from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name, char_class):
+    """Вычисляет квадратный корень."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +16,7 @@ def attack(char_name, char_class):
 
 
 def defence(char_name, char_class):
+    """Вычисляет квадратный корень."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +27,7 @@ def defence(char_name, char_class):
 
 
 def special(char_name, char_class):
+    """Вычисляет квадратный корень."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -36,6 +39,7 @@ def special(char_name, char_class):
 
 
 def start_training(char_name, char_class):
+    """Вычисляет квадратный корень."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +64,7 @@ def start_training(char_name, char_class):
 
 
 def choice_char_class():
+    """Вычисляет квадратный корень."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
@@ -81,7 +86,8 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name = input('...назови себя: ')
